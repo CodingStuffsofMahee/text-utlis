af6bef852a3826f1beaed362208d2631681a9b9d
Maheep Gupta <91566211+CodingStuffsofMahee@users.noreply.github.com>
2023-08-19 21:53:24 +0530

    unwanted navbar things removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ export const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg  m-1 rounded-4 " style={theme.navbarBg} data-bs-theme="dark">
                 <div className="container-fluid ">
-                    <a className={`navbar-brand`} style={theme.textColor} href="#">Text Utlis</a>
+                    <a className={`navbar-brand`} style={theme.textColor} href="/">Text Utlis</a>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -32,12 +32,6 @@ export const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            {/* <li className="nav-item">
-                                <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active text-white" href="#">Link</a>
-                            </li> */}
                         </ul>
                         <form className="d-flex" role="search">
                             <ColorTheme changeTheme={changeTheme} />
